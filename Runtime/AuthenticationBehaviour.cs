82063e12fd2c19803f852bea5ff63695b2db65af
subhiqudsi <pLy94T74LGHPtis>
2022-06-22 11:41:36 +0300

    setters getters

--- Runtime/AuthenticationBehaviour.cs
+++ Runtime/AuthenticationBehaviour.cs
@@ -36,7 +36,10 @@ namespace AuthenticationScope
             Debug.Log("Results retreived successfully!!");
             Debug.Log("Token retreived from Unity: " + obj);
 
-            // TamatemAuth.SetAccesToken(obj.accessToken);
+            // TamatemSDK.SetAccesToken(obj.accessToken);
+            // TamatemSDK.SetRefreshToken(obj.accessToken);
+            // TamatemSDK.SetExpiry(obj.accessToken);
+            // TamatemSDK.SetStringifiedUser(obj.accessToken);
         }
 
         void onFail() 
@@ -45,7 +48,12 @@ namespace AuthenticationScope
         }
     }
 
-       public class TamatemSDK : MonoBehaviour{
+    interface Item {
+        string type;
+        
+    }
+
+    public class TamatemSDK : MonoBehaviour{
         private string _accessToken {get; set;}
         private int _expiry{get; set;}
         private string _refreshToken {get; set; }
@@ -92,7 +100,7 @@ namespace AuthenticationScope
             _strigifiedUser = userObjectString;
         }
 
-        public static long getTime()
+        private static long _getTime()
         {
             return (long)((DateTime.Now.ToUniversalTime() - _JanFirst1970).TotalMilliseconds + 0.5);
         }
@@ -100,7 +108,7 @@ namespace AuthenticationScope
 
         public bool IsloggedIn()
         {
-           if (_accessToken == null )
+           if (_accessToken == null && _getTime() < expiry)
            {
                 return false;
            }
@@ -110,43 +118,44 @@ namespace AuthenticationScope
         }
 
 
-        // public bool RefreshToken()
-        // {
-        //     // List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
-        //     // formData.Add(new MultipartFormDataSection("field1=foo&field2=bar"));
-        //     // formData.Add(new MultipartFormFileSection("my file data", "myfile.txt"));
+        public bool RefreshToken()
+        {
+            // List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+            // formData.Add(new MultipartFormDataSection("field1=foo&field2=bar"));
+            // formData.Add(new MultipartFormFileSection("my file data", "myfile.txt"));
 
-        //     // UnityWebRequest www = UnityWebRequest.Post("https://www.my-server.com/myform", formData);
-        //     // // var json = "{\"username\":\""+username+"\", \"password\":\""+password+"\", \"email\":\""+email+"\"}";
+            // UnityWebRequest www = UnityWebRequest.Post("https://www.my-server.com/myform", formData);
+            // // var json = "{\"username\":\""+username+"\", \"password\":\""+password+"\", \"email\":\""+email+"\"}";
 
-        //     // www.SetRequestHeader("Content-Type", "application/json");
-        //     // yield return www.SendWebRequest();
+            // www.SetRequestHeader("Content-Type", "application/json");
+            // yield return www.SendWebRequest();
 
-        //     // if (www.responseCode != 200)
-        //     // {
-        //     //     Debug.Log(www.error);
-        //     // }
-        //     // else
-        //     // {
-        //     //     Debug.Log("Form upload complete!");
-        //     // }
-        // }
+            // if (www.responseCode != 200)
+            // {
+            //     Debug.Log(www.error);
+            // }
+            // else
+            // {
+            //     Debug.Log("Form upload complete!");
+            // }
+        }
 
 
+        public 
 
-        // public bool IsTokenReadyOrReferesh()
-        // {
-        //     double timestampNow = 1498122000;
-        //     DateTime fecha = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc).AddSeconds(timestamp);
-        //     if(TamatemAuth.expiry <= timestampNow)
-        //     {
-        //         return true;
-        //     }
-        //     else
-        //     {
-        //         this.RefreshToken();
-        //     }
-        // }
+        public bool IsTokenReadyOrReferesh()
+        {
+            double timestampNow = 1498122000;
+            DateTime fecha = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc).AddSeconds(timestamp);
+            if(TamatemAuth.expiry <= timestampNow)
+            {
+                return true;
+            }
+            else
+            {
+                this.RefreshToken();
+            }
+        }
     }
 
 

f18de5f658fdfba308186798cbedaf8460fb6f5d
Mohammad Salhab <mohammad.s@sitech.me>
2022-06-27 15:15:54 +0300

    commit unsaved files

--- Assets/Plugins/iOS/SourceFiles/UnityPlugin-Bridging-Header.h
+++ Assets/Plugins/iOS/SourceFiles/UnityPlugin-Bridging-Header.h
@@ -5,11 +5,5 @@
 //  Created by sanad barjawi on 15/05/2022.
 //
 
-#ifndef UnityPlugin_Bridging_Header_h
-#define UnityPlugin_Bridging_Header_h
-/**
- this or the below it, first try without import. try the sendMessage method
- */
-//#import <UnityInterface.h>
-//#import <AuthPOC/Unity/Classes/Unity/UnityInterface.h>
-#endif /* UnityPlugin_Bridging_Header_h */
+#import "UserInfoClass.h"
+#import "UnityPluginBridge.h"
